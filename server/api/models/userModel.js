4a6d3773c2a2cb18ea46df2d9a5eb5c0f6188755
eldy <eldy8888@gmail.com>
2017-12-24 00:29:32 +0800

    fix missing method in userModel

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -32,4 +32,8 @@ const UserSchema = new Schema({
   }
 })
 
+UserSchema.methods.comparePassword = function(password) {    
+ return bcrypt.compareSync(password, this.hash_password)   
+}
+
 mongoose.model('User', UserSchema)
